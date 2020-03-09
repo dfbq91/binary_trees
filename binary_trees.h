0095d272dd4aec47407d9b8b3a1195982ede1a73
dfbq91 <dfbq91@gmail.com>
2020-03-09 16:05:15 -0500

    tasks from 0 to 3 and betty fixed

--- binary_trees.h
+++ binary_trees.h
@@ -4,7 +4,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -15,16 +14,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
