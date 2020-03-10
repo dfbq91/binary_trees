4e4ecadb08a96d38fdbaf652fea7fb7b5c5c675a
dfbq91 <dfbq91@gmail.com>
2020-03-09 19:36:10 -0500

    added condition if given node is NULL

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: pointer to the node to check
- * Return: void
+ * Return: 1 if the given node is leaf, otherwise 0
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
