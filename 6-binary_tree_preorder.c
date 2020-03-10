6e4ad16fe14cfb6f8292eecc83bf25e3dfc0634b
dfbq91 <dfbq91@gmail.com>
2020-03-09 19:36:46 -0500

    just added one condition. Need to be completed

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function that goes through a
+ * binary tree using pre-order traversal
+ * @node: pointer to the root node of the tree to traverse
+ * Return: void
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+{
+	if (tree == NULL || func == NULL)
+		return (NULL);
+
+	Will continue;
+}
