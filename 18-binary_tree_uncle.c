b131b08f2f80207ca1baf8e9cdea8f2d0eefd01f
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-11 13:38:15 -0500

    uncle betty style corrected

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,21 +7,22 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    int val1, val2;
-    if (!node || !node->parent->parent)
-        return (NULL);
-    if (node->parent->parent)
-    {
-        if (node->parent->parent->left)
-        {
-            val1 = node->parent->n;
-            val2 = node->parent->parent->left->n;
-            if (val1 == val2)
-            {
-                return (node->parent->parent->right);
-            }
-            return (node->parent->parent->left);
-        }
-    }
-    return (NULL);
+	int val1, val2;
+
+	if (!node || !node->parent->parent)
+		return (NULL);
+	if (node->parent->parent)
+	{
+		if (node->parent->parent->left)
+		{
+			val1 = node->parent->n;
+			val2 = node->parent->parent->left->n;
+			if (val1 == val2)
+			{
+				return (node->parent->parent->right);
+			}
+			return (node->parent->parent->left);
+		}
+	}
+	return (NULL);
 }
