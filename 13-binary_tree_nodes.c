5425bb1192b4ef3bc4bb3140fabcda1b67e2c892
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-11 13:27:19 -0500

    nodes betty style corrected

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,16 +5,16 @@
 *@count: the counter
 *Return: something
 */
-void nodes(binary_tree_t * node, size_t *count)
+void nodes(binary_tree_t *node, size_t *count)
 {
-    if (node->left || node->right)
-    {
-        *count += 1;
-        if (node->left)
-            nodes(node->left, count);
-        if (node->right)
-            nodes(node->right, count);
-    }
+	if (node->left || node->right)
+	{
+		*count += 1;
+		if (node->left)
+			nodes(node->left, count);
+		if (node->right)
+			nodes(node->right, count);
+	}
 }
 
 
@@ -26,23 +26,23 @@ void nodes(binary_tree_t * node, size_t *count)
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t count = 0;
+	size_t count = 0;
 
 	if (tree == NULL)
 		return (0);
 
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        count += 1;
-    }
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		count += 1;
+	}
 
 	if (tree->left != NULL)
-    {
+	{
 		nodes(tree->left, &count);
-    }
+	}
 	if (tree->right != NULL)
-    {
-        nodes(tree->right, &count);
-    }
+	{
+		nodes(tree->right, &count);
+	}
 	return (count);
 }
