dcdda1d3cba14e9c6a38b74258543589ca970896
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-11 13:22:17 -0500

    height meassure corrected

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -25,7 +25,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 size_t height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1);
+		return (0);
 	return (1 + max(height(tree->left), height(tree->right)));
 
 }
