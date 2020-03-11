14e7e756e2d81e0244c5f4556aa8232d179d884a
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-11 13:24:52 -0500

    depth betty style corrected

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-size_t max_depth(const binary_tree_t *tree);
 /**
 *binary_tree_depth - returns the node depth
 *@tree: the node to check
@@ -9,28 +8,15 @@ size_t max_depth(const binary_tree_t *tree);
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    const binary_tree_t *node = tree;
-    size_t levels = 0;
-    if (!tree)
-        return (0);
-    while (node->parent)
-    {
-        node = node->parent;
-        levels++;
-    }
-    return (levels);    
-}
+	const binary_tree_t *node = tree;
+	size_t levels = 0;
 
-size_t max_depth(const binary_tree_t *tree)
-{
-    size_t ldepth, rdepth;
-
-    if (!tree)
-        return (0);
-    ldepth = max_depth(tree->left);
-    rdepth = max_depth(tree->right);
-    if (ldepth > rdepth)
-        return (ldepth + 1);
-    else
-        return (rdepth + 1);
+	if (!tree)
+		return (0);
+	while (node->parent)
+	{
+		node = node->parent;
+		levels++;
+	}
+	return (levels);
 }
